64de672249da508c628cdb1b7d25ad929f7227a5
Marwa youssef106 <marwaadel713@gmail.com>
2023-05-02 05:42:33 -0700

    Task 8

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,13 +4,13 @@
  * @dest: char to check
  * @src: char to check
  * Return: 0 is success
- * */
+ */
 char *_strcpy(char *dest, char *src)
 {
-int a;
+	int a;
 
-for (a = 0; src[a] != '\0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
 		dest[a] = src[a];
 	dest[a] = '\0';
-	return (dset);
+	return (dest);
 }
