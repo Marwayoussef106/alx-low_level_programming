fdb57b752cb7f23458c79c55de526a560c5d5293
Marwayoussef106 <marwaadel713@gmail>
2023-08-17 01:09:48 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOH_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name : dog's name
+ * @aga : dog's aga
+ * @owner : owner's name
+ * /
+ * Description : just a long dog struct in a big kitty world
+ * /
+ * struct dog
+ {
+    char *name;
+    float aga;
+    char *owner;
+ };
+ #endif
+ ~
+ ~
